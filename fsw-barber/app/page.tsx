bd03630ae72c06e53668d3aaeda77fce54e8ac54
Valfran <valfran-goncalves@hotmail.com>
2025-07-05 18:00:27 -0300

    chore: update shadcn  theme

--- fsw-barber/app/page.tsx
+++ fsw-barber/app/page.tsx
@@ -2,8 +2,10 @@ import { Button } from "./_components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Button variant="outline">Click me</Button>
+    <div >
+      <Button>
+        Click me
+      </Button>
     </div>
   );
 }
