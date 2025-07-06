4b738e5ae298180c78a4bf12c1cd7c10c0b5355b
Valfran <valfran-goncalves@hotmail.com>
2025-07-05 21:35:16 -0300

    feat: add Header component and banner

--- fsw-barber/app/page.tsx
+++ fsw-barber/app/page.tsx
@@ -1,15 +1,32 @@
 import { Button } from "./_components/ui/button";
+import Header from "./_components/header";
+import { Input } from "./_components/ui/input";
+import { SearchIcon } from "lucide-react";
+import Image from "next/image";
 
-export default function Home() {
+const Home = () => {
   return (
     <div >
-      <Button>
-        Click me
-      </Button>
+      {/* header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Valfran!</h2>
+        <p>Sábado, 05 de julho</p>
+        <div className="flex items-center gap-2 mt-6">
+          <Input placeholder="Faça sua busca" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative w-full h-[150px] mt-6 rounded-xl">
+          <Image alt="agende nos melhores com Fsw Barber"
+            src="/Banner.png"
+            fill
+            className="object-content" />
+        </div>
+      </div>
     </div>
   );
 }
-
-export {
-  Button,
- } from "./_components/ui/button";
+export default Home;
