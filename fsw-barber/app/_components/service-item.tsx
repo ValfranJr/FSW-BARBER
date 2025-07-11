45124d79b29c23e498192c096360651a657fd744
Valfran <valfran-goncalves@hotmail.com>
2025-07-11 11:39:10 -0300

    feat: add "see bookings" button on booking sucess alert

--- fsw-barber/app/_components/service-item.tsx
+++ fsw-barber/app/_components/service-item.tsx
@@ -21,6 +21,7 @@ import { getBookings } from "../_actions/get-bookings"
 import { Dialog, DialogContent } from "./ui/dialog"
 import SignInDialog from "./sign-in-dialog"
 import BookingSummary from "./booking-summary"
+import { useRouter } from "next/navigation"
 
 interface ServiceItemProps {
   service: BarbershopService
@@ -79,8 +80,9 @@ const getTimeList = ({ bookings, selectedDay }: GetTimeListProps) => {
 }
 
 const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
-  const [signInDialogIsOpen, setSignInDialogIsOpen] = useState(false)
   const { data } = useSession()
+  const router = useRouter()
+  const [signInDialogIsOpen, setSignInDialogIsOpen] = useState(false)
   const [selectedDay, setSelectedDay] = useState<Date | undefined>(undefined)
   const [selectedTime, setSelectedTime] = useState<string | undefined>(
     undefined,
@@ -137,7 +139,15 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
         date: selectedDate,
       })
       handleBookingSheetOpenChange()
-      toast.success("Reserva realizada com sucesso!")
+      toast.success("Reserva realizada com sucesso!",{
+        action: {
+          label: "Ver agendamento",
+          onClick: () => {
+            router.push("/bookings")
+          },
+        }
+      })
+
     } catch (error) {
       console.log(error)
       toast.error("Ocorreu um erro ao realizar a reserva!")
